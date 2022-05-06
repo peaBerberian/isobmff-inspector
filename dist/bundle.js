6adf414d626a1a1838109fc2c748b6dc7db93be1
Paul Berberian <pea.berberian@gmail.com>
2022-05-06 23:15:38 +0200

    update build

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.inspectISOBMFF = factory());
-}(this, (function () { 'use strict';
+})(this, (function () { 'use strict';
 
   /**
    * Translate groups of 2 big-endian bytes to Integer (from 0 up to 65535).
@@ -1305,4 +1305,4 @@
 
   return parseBoxes;
 
-})));
+}));
